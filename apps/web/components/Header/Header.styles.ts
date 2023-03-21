60d52c0b7f597544427c699670c5c747a7f9427d
Nicolas Le Borgne <nleborgne.hrteam@lfdj.com>
2023-03-21 19:31:54 +0100

    update background color

--- apps/web/components/Header/Header.styles.ts
+++ apps/web/components/Header/Header.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 10vh;
-  background-color: #001367;
+  background-color: red;
   box-sizing: border-box;
   text-align: center;
 `;
